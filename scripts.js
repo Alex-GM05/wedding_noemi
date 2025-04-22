84f879ebc85eb3f3cab3d89cfc86c94819223022
Alex-GM <oalejandro.garcia.maciasipn@gmail.com>
2025-04-21 19:28:41 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD1aQZuIhcg7H4nzrA9cR-g_aFML98Tfwg",
   authDomain: "wedding-noemi-a30fe.firebaseapp.com",
